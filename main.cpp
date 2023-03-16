e2f9b993230b7a89785a992d9c27063ce0caa46f
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2023-03-17 01:16:50 +0800

    Amin panjang

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main() {
     std::cout << "Hello, Crush Baru" << std::endl;
     std::cout << "Alesya is puteri lilin" << std::endl;
     std::cout << "Semoga saya hidup bahagia" << std::endl;
-    std::cout << "Amin" << std::endl;
+    std::cout << "Amin ya rabbalalamin" << std::endl;
     return 0;
 }
