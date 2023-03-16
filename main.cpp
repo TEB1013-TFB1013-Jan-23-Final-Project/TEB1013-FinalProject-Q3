f3930f9756fd375fdb27417b54ab7a367630f058
Wan Alesya Binti Wan Azlan <wan_22009116@utp.edu.my>
2023-03-17 00:02:00 +0800

    Aku ada crush baru u

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Crush Baru" << std::endl;
     std::cout << "Alesya is puteri lilin" << std::endl;
     return 0;
 }
