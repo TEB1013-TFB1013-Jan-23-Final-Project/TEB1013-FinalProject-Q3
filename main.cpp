5f29dca0bc922c506a5924b22648323fa4edfef8
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2023-03-21 13:56:23 +0800

    Start from scratch

--- main.cpp
+++ main.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
 
-template<typename T>
-T getInput(const char* message) {
-    T input;
-    std::cout << message << std::endl;
-    std::cin >> input;
-    return input;
-}
-
 int main() {
-    std::cout << "Hello, Crush Baru" << std::endl;
-    std::cout << "Alesya is puteri lilin" << std::endl;
-    std::cout << "Semoga saya hidup bahagia" << std::endl;
-    std::cout << "Amin three times" << std::endl;
-    std::cout << "Amin ya rabbalalamin" << std::endl;
-    std::cout << "Nak bf kaya u" << std::endl;
     return 0;
 }
